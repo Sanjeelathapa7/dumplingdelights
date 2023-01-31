b0c46632de8cfb76a68a683332a114cc95836e9d
Sanjeelathapa7 <sanjeelathapamagar1234@gmail.com>
2023-01-31 21:44:37 +0545

    khaltipayment added

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
+
+import 'Payment/KhaltiTest.dart';
+
+
+void main() {
+  runApp(const Myapp());
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]);
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return KhaltiScope(
+        publicKey: 'test_public_key_5a22ad67e707441b8362fc7bed556a8d',
+        enabledDebugging: true,
+        builder: (context, navKey) {
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: KhaltiPayment(),
+              navigatorKey: navKey,
+              localizationsDelegates: const [
+                KhaltiLocalizations.delegate,
+              ]);
+        });
+  }
+}
